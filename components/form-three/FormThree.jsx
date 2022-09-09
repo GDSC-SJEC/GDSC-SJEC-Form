f9b7813e3e23dcf4baf3161cba1b08dfb22619d0
Godnon Dsilva <godnondsilva@gmail.com>
2022-09-10 01:48:09 +0530

    Add introduce yourself text

--- components/form-three/FormThree.jsx
+++ components/form-three/FormThree.jsx
@@ -26,6 +26,9 @@ const FormThree = () => {
 					isPaused={false}
 				/>
 				<h2>Your response has been recorded!</h2>
+				<p style={{ paddingBottom: '2rem' }}>
+					Don&rsquo;t forget to introduce yourself on our discord server.
+				</p>
 				<AnchorStyles href='mailto:gdsc@sjec.ac.in'>
 					Made a mistake? Contact us
 				</AnchorStyles>
